79748649f14e0b2a5248a8d571f5c091225406ac
Mukul Mehta <mukulmehta022@gmail.com>
2026-09-21 07:04:38 +0530

    day10 completed

---
+++ day10/second.py
@@ -0,0 +1,10 @@
+nums=[[5,20,3],[7,-10,9],[1,-52,6]]
+def upper_triangle(nums):
+    row=len(nums)
+    col=len(nums[0])
+    for i in range(row):
+        for j in range(col):
+            if i>j :
+                nums[i][j]="*"
+upper_triangle(nums)
+print(nums)
---
+++ day9/first.py
@@ -0,0 +1,36 @@
+nums=[[5,8,9],[10,7,6],[3,1,2]]
+nums1=[[5,8,9,10],[10,7,6,8],[3,1,2,7]]
+def set_zeroes(matrix):
+    rows=len(matrix)
+    columns=len(matrix[0])
+    rowtrack=[0 for _ in range(rows)]
+    columnstrack=[0 for _ in range(columns)]
+    for i in range(rows):
+        for j in range(columns):
+            if matrix[i][j]==0:
+                rowtrack[i]=-1
+                columnstrack[j]=-1
+    for i in range(rows):
+        for j in range(columns):
+            if rowtrack[i]==-1 or columnstrack[j]==-1:
+                matrix[i][j]=0
+def display(matrix):
+    rows=len(matrix)
+    columns=len(matrix[0])
+    for i in range(rows):
+        for j in range(columns):
+            print( matrix[i][j] , end=" ")
+        print()
+def transpose(matrix):
+    rows=len(matrix)
+    columns=len(matrix[0])
+    result=[[0]*rows for _ in range(columns)]
+    for i in range(rows):
+        for j in range(columns):
+            result[j][i]=matrix[i][j]
+    return result
+set_zeroes(nums)
+display(nums)
+print("Transpose")
+result=transpose(nums1)
+display(result)
